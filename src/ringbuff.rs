6ea0da8ade4b81c0d83a0055253666e19e73c38f
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2022-07-02 19:39:05 +0200

    Fixed index out ouf bounds error

--- src/ringbuff.rs
+++ src/ringbuff.rs
@@ -4,9 +4,9 @@ use std::vec::Vec;
 #[derive(Debug)]
 pub struct RingBuff<T: Copy> {
     pub data: Vec<T>,
-    pub write_offset: usize,
-    pub read_offset: usize,
-    pub wrap_flag: bool,
+    write_offset: usize,
+    read_offset: usize,
+    wrap_flag: bool,
 }
 impl<T: Copy + Debug> RingBuff<T> {
     pub fn new(initial_size: usize, initial_value: T) -> Self {
@@ -24,33 +24,36 @@ impl<T: Copy + Debug> RingBuff<T> {
         }
     }
 
-    pub fn append(&mut self, appendage: T) -> Result<(), ()> {
+    pub fn append(&mut self, appendage: T) {
         self.data[self.write_offset] = appendage;
 
-        if self.write_offset < self.data.len() {
+        if self.write_offset < self.data.len() - 1 {
             self.write_offset += 1;
         } else {
             self.write_offset = 0;
             self.wrap_flag = true;
         }
-
-        return Ok(());
     }
 
     pub fn reduce(&mut self) -> Result<T, ()> {
         let temp = self.data[self.read_offset];
 
-        if self.read_offset < self.data.len() && self.read_offset < self.write_offset {
-            self.read_offset += 1;
+        if self.read_offset < self.write_offset || self.wrap_flag {
+            if self.read_offset < self.data.len() - 1 {
+                self.read_offset += 1;
+            } else {
+                self.wrap_flag = false;
+                self.read_offset = 0;
+            }
         } else {
             return Err(());
         }
 
-        if self.wrap_flag {
-            self.read_offset = 0;
-            self.wrap_flag = false;
-        }
-
         return Ok(temp.clone());
+    }
+
+    pub fn reset_offset(&mut self) {
+        self.read_offset = 0;
+        self.write_offset = 0;
     }
 }
