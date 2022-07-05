73d30d395e9c54fdbe88d7a28dcbe9bdc391c6d2
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2022-07-05 14:34:43 +0200

    Moved variables inside thread scope

--- src/main.rs
+++ src/main.rs
@@ -12,32 +12,34 @@ fn main() {
 
     serial.start().expect("Listener already initialized!");
 
-    let mut bytecounter = 0;
-    let data_lock = serial.shared.clone();
-    let mut temp = ringbuff::RingBuff::new(14, 0);
+    let decoder = spawn(move || {
+        let data_lock = serial.shared.clone();
+        let mut temp = ringbuff::RingBuff::new(14, 0);
+        let mut bytecounter = 0;
 
-    let decoder = spawn(move || loop {
-        {
-            let mut data = data_lock.lock().unwrap();
+        loop {
+            {
+                let mut data = data_lock.lock().unwrap();
 
-            while let Ok(i) = data.buff.reduce() {
-                if i == 0xa5 {
+                while let Ok(i) = data.buff.reduce() {
+                    if i == 0xa5 {
+                        bytecounter = 0;
+                    }
+                    bytecounter += 1;
+                    temp.append(i);
+                }
+
+                if bytecounter >= 14 {
+                    let decoded_frame = eldecode::EltakoFrame::from_vec(&temp.data[0..14]);
+                    if let Ok(frame) = decoded_frame {
+                        info!("{}", frame.explain());
+                    }
+                    temp.reset_offset();
                     bytecounter = 0;
                 }
-                bytecounter += 1;
-                temp.append(i);
             }
-
-            if bytecounter >= 14 {
-                let decoded_frame = eldecode::EltakoFrame::from_vec(&temp.data[0..14]);
-                if let Ok(frame) = decoded_frame {
-                    info!("{}", frame.explain());
-                }
-                temp.reset_offset();
-                bytecounter = 0;
-            }
+            std::thread::sleep(std::time::Duration::from_millis(1));
         }
-        std::thread::sleep(std::time::Duration::from_millis(1));
     });
 
     decoder.join().expect("Could not join decoder");
