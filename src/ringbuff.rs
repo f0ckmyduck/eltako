2639c8344a1980a25119cacf5d3f8beed3cbfa27
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2022-07-05 17:16:49 +0200

    Added functions to the ringbuffer api

--- src/ringbuff.rs
+++ src/ringbuff.rs
@@ -3,7 +3,7 @@ use std::vec::Vec;
 
 #[derive(Debug)]
 pub struct RingBuff<T: Copy> {
-    pub data: Vec<T>,
+    data: Vec<T>,
     write_offset: usize,
     read_offset: usize,
     wrap_flag: bool,
@@ -35,10 +35,19 @@ impl<T: Copy + Debug> RingBuff<T> {
         }
     }
 
+    pub fn readable(&self) -> bool {
+        if self.read_offset < self.write_offset
+            || self.wrap_flag && self.read_offset > self.write_offset
+        {
+            return true;
+        }
+        false
+    }
+
     pub fn reduce(&mut self) -> Result<T, ()> {
         let temp = self.data[self.read_offset];
 
-        if self.read_offset < self.write_offset || self.wrap_flag {
+        if self.readable() {
             if self.read_offset < self.data.len() - 1 {
                 self.read_offset += 1;
             } else {
@@ -52,8 +61,41 @@ impl<T: Copy + Debug> RingBuff<T> {
         return Ok(temp.clone());
     }
 
+    pub fn reduce_slice(&mut self, size: usize) -> Result<Vec<T>, ()> {
+        let mut temp = Vec::with_capacity(size);
+
+        for _ in 0..size {
+            let current = match self.reduce() {
+                Ok(x) => x,
+                Err(()) => {
+                    return Err(());
+                }
+            };
+
+            temp.push(current);
+        }
+
+        Ok(temp)
+    }
+
     pub fn reset_offset(&mut self) {
         self.read_offset = 0;
         self.write_offset = 0;
+    }
+
+    pub fn get_write_offset(&self) -> usize {
+        self.write_offset
+    }
+
+    pub fn get_read_offset(&self) -> usize {
+        self.read_offset
+    }
+
+    pub fn set_read_offset(&mut self, offset: usize) {
+        self.read_offset = offset;
+    }
+
+    pub fn get_vec(&self) -> Vec<T> {
+        self.data.clone()
     }
 }
